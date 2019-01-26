f7a52b01e9ec24d29299d7beb1db419fe686ac37
Julio Felipe Angelini <julio.angelini@gmail.com>
2019-01-26 17:09:42 -0200

    Build scene progression

---
+++ src/Trigger1.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Trigger1_body_entered(body):
+	print("aaaaaaaaaaaaaaaaaaaaaaaaa ", body.is_in_group("player"))
+	if body.is_in_group("player") and body.story_index == 0:
+		body.story_index = 1
+		for element in get_tree().get_nodes_in_group("step1"):
+			element.hide()
+		for element in get_tree().get_nodes_in_group("step2"):
+			element.show()
---
+++ src/Trigger2.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Trigger2_body_entered(body):
+	print("bbbbbbbbbbbbbbbbbbbbbbbbbb ", body.story_index)
+	if body.is_in_group("player") and body.story_index == 1:
+		body.story_index = 2
+		get_tree().change_scene("res://scenes/FallingInWell.tscn")
