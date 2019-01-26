f5bffd012a2a83bbecd9db8ae6f2c730cd8052f2
Julio Felipe Angelini <julio.angelini@gmail.com>
2019-01-26 20:39:07 -0200

    Derp way of making NPCs follow you

--- src/NPC.gd
+++ src/NPC.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var is_following_player = false
+var playerDistX = 0
+var playerDistY = 0
 # var b = "textvar"
 
 func _ready():
@@ -10,11 +12,19 @@ func _ready():
 
 func _process(delta):
 	if is_following_player:
-		var player = get_tree().get_nodes_in_group("player")[0]
+		var player = get_player()
 		var parent = get_parent()
-		parent.position.x = player.x
-		parent.position.y = player.y
+		parent.position.x = player.position.x - playerDistX
+		parent.position.y = player.position.y - playerDistY
+
+func get_player():
+	return get_tree().get_nodes_in_group("player")[0]
 
 func _on_Area2D_body_entered(body):
 	if not is_following_player and body.is_in_group("player"):
 		is_following_player = true
+		var player = get_player()
+		var parent = get_parent()
+		playerDistX = player.position.x - parent.position.x
+		playerDistY = player.position.y - parent.position.y
+
