80c5b6aaa1c15a7ca21c8749741dabf187b1b4c6
@virgulaleal <aqueleleal@gmail.com>
2019-01-26 22:15:55 -0200

    Polindo o fundo e a janela

--- src/Trigger1.gd
+++ src/Trigger1.gd
@@ -17,6 +17,7 @@ func _ready():
 
 func _on_Trigger1_body_entered(body):
 	if body.is_in_group("player") and body.story_index == 0:
+		ProjectSettings.set_setting("rendering/environment/default_clear_color", Color("ff000000"))
 		body.story_index = 1
 		for element in get_tree().get_nodes_in_group("step1"):
 			element.hide()
