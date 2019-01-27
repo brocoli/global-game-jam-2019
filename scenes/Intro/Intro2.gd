eecccb048c9181fd6a07e6651e9536d41fcd3300
Julio Felipe Angelini <julio.angelini@gmail.com>
2019-01-27 10:58:31 -0200

    Fixed intro

--- scenes/Intro/Intro2.gd
+++ scenes/Intro/Intro2.gd
@@ -1,20 +1,19 @@
 extends AnimatedSprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	play()
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-
-func _on_AnimatedSprite_animation_finished():
+	$Timer.start()
+	yield($Timer, "timeout")
+	$Timer.start()
+	yield($Timer, "timeout")
+	play("1")
+	$Timer.start()
+	yield($Timer, "timeout")
+	play("2")
+	$Timer.start()
+	yield($Timer, "timeout")
+	play("3")
+	$Timer.start()
+	yield($Timer, "timeout")
 	$Timer.start()
 	yield($Timer, "timeout")
 	get_tree().change_scene("res://scenes/Main.tscn")
